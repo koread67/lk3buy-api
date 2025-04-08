6608c19a2c3722d112d7559d75e9bf152bad1213
LK <lk7208@hanmail.net>
2025-04-09 01:09:29 +0900

    fix: bind flask to Render port

--- etf_buy_signal_api.py
+++ etf_buy_signal_api.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 # Flask API Server
 # ?�기??etf_buy_signal_api 코드 붙여?�으?�요
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render에서 제공하는 포트
+    app.run(host='0.0.0.0', port=port)
